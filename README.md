7db252dd2dc834421a12beaa566790c4db5c7908
Frank Hampus Weslien <frankhampusweslien@gmail.com>
2024-09-14 16:15:19 +0200

    Simple counter app

---
+++ README.md
@@ -0,0 +1,3 @@
+# Iced Counter
+
+Simple counter app built with Iced. I made this to learn how to use Iced and to have a simple example to refer to in the future.
---
+++ src/main.rs
@@ -0,0 +1,66 @@
+use iced::widget::{button, column, text};
+use iced::{Alignment, Element, Sandbox, Settings};
+
+pub fn main() -> iced::Result {
+    Counter::run(Settings::default())
+}
+
+struct Counter {
+    value: i32,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Message {
+    Increment,
+    Decrement,
+}
+
+impl Sandbox for Counter {
+    type Message = Message;
+
+    fn new() -> Self {
+        Self { value: 0 }
+    }
+
+    fn title(&self) -> String {
+        String::from("Counter - Iced")
+    }
+
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::Increment => {
+                self.value += 1;
+            }
+            Message::Decrement => {
+                self.value -= 1;
+            }
+        }
+    }
+
+    fn view(&self) -> Element<Message> {
+        column![
+            button("Increment").on_press(Message::Increment),
+            text(self.value).size(50),
+            button("Decrement").on_press(Message::Decrement)
+        ]
+        .padding(20)
+        .align_items(Alignment::Center)
+        .into()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_counts_properly() {
+        let mut counter = Counter { value: 0 };
+
+        counter.update(Message::Increment);
+        counter.update(Message::Increment);
+        counter.update(Message::Decrement);
+
+        assert_eq!(counter.value, 1);
+    }
+}
